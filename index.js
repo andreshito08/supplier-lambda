f7dd5c396c664d69e79688d244ec9316a521ad2c
Andres Moreno <andres.moreno@laika.pet>
2023-03-28 16:37:10 -0500

    se agrega validacion de key en la cabecera

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
+require('dotenv').config();
 const { SupplierService,Supplier } = require('./app');
 
 exports.handler = async(event) => {
     try {
-        const {name,email} = event.body;
-        if (!name) {
-          return {
-            statusCode: 400,
-            message: 'name requerido',
-          };
-        }else if (!email) {
-          return {
-            statusCode: 400,
-            message: 'email requerido',
-          };
-        }
-        const supplier = new Supplier(name, email);
-        await SupplierService.createSupplier(supplier);
+      const validateKey = authMiddleware(event);
+      
+      if(!validateKey){
         return {
-          statusCode: 200,
-          message: 'Supplier create.'
-        };
-      } catch (error) {
-        console.error('Error creating Supplier', error);
-        return {
-          statusCode: 500,
-          message: 'Error creating supplier', 
-          error: error,
+          statusCode: 401,
+          message: 'No autorizado'
         };
       }
+
+      const {name,email} = event.body;
+      if (!name) {
+        return {
+          statusCode: 400,
+          message: 'name requerido',
+        };
+      }else if (!email) {
+        return {
+          statusCode: 400,
+          message: 'email requerido',
+        };
+      }
+      const supplier = new Supplier(name, email);
+      await SupplierService.createSupplier(supplier);
+      return {
+        statusCode: 201,
+        message: 'Supplier create.'
+      };
+    } catch (error) {
+      console.error('Error creating Supplier', error);
+      return {
+        statusCode: 500,
+        message: 'Error creating supplier', 
+        error: error,
+      };
+    }
 };
+
+const authMiddleware = (req) => {
+  const authHeader = req.headers.api_key;
+  if (authHeader && authHeader == process.env.API_KEY) {
+      return true;
+  }
+}
